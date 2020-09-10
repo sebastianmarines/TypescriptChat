d933eeb61c17308dda5b5c746f581fc9320b42b7
Sebastian Marines <sebastian0marines@gmail.com>
2020-09-10 11:11:57 -0500

    Clean project

--- frontend-r/src/App.tsx
+++ frontend-r/src/App.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -20,7 +18,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+    )
+  }
 }
 
-export default App;
