8801ae56c964aa44a5f0255c61b318d8172b0201
Sebastian Marines <sebastian0marines@gmail.com>
2020-09-10 12:40:53 -0500

    Create message component

---
+++ frontend-r/src/components/Message.tsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+import IMessage from "../interfaces/Message.interface";
+import { Row, Col } from "reactstrap";
+
+export default class Message extends Component<IMessage, {}> {
+  render() {
+    let { content, sender, timestamp} = this.props;
+    return (
+      <Row className="my-2 border-bottom p-2">
+        <Col xs="10" md="11">
+          <span className="font-weight-bold">{sender}: </span>
+          <span>{content}</span>
+        </Col>
+        <Col xs="1">
+          <span className="small text-muted">{timestamp}</span>
+        </Col>
+      </Row>
+    );
+  }
+}
